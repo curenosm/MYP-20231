fcbd2151c4bfc50eecda8f87c01c9e4a2db606b2
Alcantara Estrada Kevin <alcantara_estrada_kevin@ciencias.unam.mx>
2022-09-18 01:21:25 -0500

    Se trabajo en los estados del robot

--- practicas/practica_2/src/p02/estados/ModoEntregarComida.java
+++ practicas/practica_2/src/p02/estados/ModoEntregarComida.java
@@ -1,46 +1,57 @@
 package p02.estados;
 
+import p02.modelos.Robot;
+
 public class ModoEntregarComida implements EstadoRobot {
+    Robot robot;
+
+   
+    public ModoEntregarComida(Robot robot){
+        this.robot=robot;
+    }
 
     @Override
     public void suspender() {
-        // TODO Auto-generated method stub
+        System.out.println("No me puedo suspender");
         
     }
 
     @Override
     public void atender() {
-        // TODO Auto-generated method stub
+        System.out.println("Ya te estoy atendiendo");
         
     }
 
     @Override
     public void cocinar() {
-        // TODO Auto-generated method stub
+        System.out.println("Actualmente ya he cocicnado todo");
         
     }
 
     @Override
     public void apagar() {
-        // TODO Auto-generated method stub
+        System.out.println("Para apagarme debo estar suspendido");
         
     }
 
     @Override
     public void caminar() {
-        // TODO Auto-generated method stub
+        System.out.println("No se me permite caminar en estos momentos");
         
     }
 
     @Override
     public void entregarComida() {
-        // TODO Auto-generated method stub
+        System.out.println("Espero disfrute su comida");
+        System.out.println("Entrando en modo suspendido");
+         robot.asignarEstado(robot.getModoSuspendido()); 
+
         
     }
 
     @Override
     public void encender() {
-        // TODO Auto-generated method stub
+        System.out.println("Ya estoy encendido");
         
     }
     
