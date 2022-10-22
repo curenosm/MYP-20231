007ec6fe3bccc0e0a66bc1d5cb6d2be7a789ffd1
Misael Cureño <curenosm@gmail.com>
2022-10-22 00:11:28 -0500

    Avances Main

--- proyectos/proyecto_1/src/main/java/com/bettercodesaul/Main.java
+++ proyectos/proyecto_1/src/main/java/com/bettercodesaul/Main.java
@@ -6,5 +6,22 @@ import static com.bettercodesaul.util.Printer.*;
 public class Main {
   public static void main(String[] args) {
     success(property("version"));
+
+    if (args.length != 0) {
+      if (args[0] != null)
+        if (args[0].equals("server")) startServer();
+        else startClient();
+    } else {
+      startClient();
+    }
+
+  }
+
+  public static void startClient() {
+    info("Cliente iniciado");
+  }
+
+  public static void startServer() {
+    info("Servidor iniciado");
   }
 }
